c5c4da96cd868ecd5d2003155247ef6c5f3bbbd5
martin-raphael <martinraphael254@gmail.com>
2024-11-22 03:38:08 +0300

    first weather app using apis

---
+++ src/api.js
@@ -0,0 +1,11 @@
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+export const geoApiOptions = {
+  method: "GET",
+  headers: {
+    "x-rapidapi-key": "b1550d5f52msh7f6113a840b11c0p1bf34djsn2b0b3beaff9f",
+    "x-rapidapi-host": "wft-geo-db.p.rapidapi.com",
+  },
+};
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "713a5daac716825c7519070b74202052";
